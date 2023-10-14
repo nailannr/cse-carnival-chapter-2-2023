d0a715f08d1a9f74dc713113bab48602147800d5
nailannr <nailanausheen5@gmail.com>
2023-10-15 04:55:53 +0600

    update

--- reachout/src/App.js
+++ reachout/src/App.js
@@ -1,16 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Home from "./components/Home/Home";
+import Expertlog from "./components/Login/Expertlog";
+import Expertsign from "./components/Signup/Expertsign";
+
+import {auth} from "./firebase";
 
 import './App.css';
-import ExpertSignup from './components/ExpertSignup';
-import UserSignup from './components/UserSignup';
-import React from 'react';
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((user)=> {
+      if(user){
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
+
   return (
     <div className="App">
-      <ExpertSignup/>
-      <UserSignup/>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Expertlog />} />
+          <Route path="/signup" element={<Expertsign />} />
+          <Route path="/" element={<Home name={userName} />} />
+        </Routes>
+      </Router>
     </div>
   );
+  
 }
 
 export default App;
--- reachout/src/firebase.js
+++ reachout/src/firebase.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyA7FJ5-QMCkf9irUXJsBzrGRT3Qz_2N6OA",
   authDomain: "expert-signup.firebaseapp.com",
@@ -14,8 +12,7 @@ const firebaseConfig = {
   appId: "1:889944531565:web:d6db82786cf62c565d5564"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth=getAuth();
+export  const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app);
 
-export default {app, auth};
+//export default { app,auth};
